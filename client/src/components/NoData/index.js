fe81f1769a7577d4470f384e683d0b563ca59524
Mark Fisher <mfisher36@gmail.com>
2019-01-21 07:41:26 -0500

    didn't save data page

--- client/src/components/NoData/index.js
+++ client/src/components/NoData/index.js
@@ -6,7 +6,6 @@ function NoData(props) {
     <div className="text-center">
       <h3 className="no-data"> You do not have any {props.category} data for today. </h3>
       <i className="fas fa-plus-square mt-2"></i>
-      {props.form}
     </div>
 
 
