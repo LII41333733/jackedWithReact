c645245a464581c941baf0137c0e7cf5ed90270c
Mark Fisher <mfisher36@gmail.com>
2019-01-31 14:17:30 -0500

    saving last copy before final

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(routes);
 
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/jackedWithReact");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/jackedwithreact");
 
 // Start the API server
 app.listen(PORT, function() {
